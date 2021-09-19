30cd1f6008509db3f034c26e3fd5bfe529d6f12f
Alexander Mathew <amathew993@gmail.com>
2021-09-19 15:22:29 -0700

    added readImageHeader

--- proj1/Q1.cpp
+++ proj1/Q1.cpp
@@ -8,32 +8,36 @@ using namespace std;
 
 
 // Function declarations
-void subSampleImage(ImageType originalImage, ImageType& newImage, int factor);
+void subSampleImage(ImageType originalImage, int factor);
 int readImage(char fname[], ImageType& image);
 int writeImage(char fname[], ImageType& image);
+int readImageHeader(char fname[], int& N, int& M, int& Q, bool& type);
 
 int main(int argc, char** argv) {
-	
+
 
 }
 
 // function implementations
-void subSampleImage(ImageType originalImage,ImageType& newImage, int factor){
-
-	int orig_rows, orig_cols, orig_qlvls;
-	int sampled_rows, sampled_cols;
+void subSampleImage(ImageType originalImage, ImageType newImage, int factor){
+	int orig_rows, orig_cols, orig_qlvl;
+	int sampled_rows = 0;
+	int sampled_cols = 0;
 	int val;
 
 	// get image info
-	originalImage.getImageInfo(orig_rows, orig_cols, orig_qlvls);
-
+	originalImage.getImageInfo(orig_rows, orig_cols, orig_qlvl);
+	
 	// iterate over image
 	for(int i = 0; i < orig_cols; i += factor){
-		for(int j = 0; j < orig_rows; j+= factor){
+		for(int j = 0; j < orig_rows; j += factor){
 			originalImage.getPixelVal(i, j, val);
 			newImage.setPixelVal(sampled_rows, sampled_cols, val);
+			
 		sampled_rows++;
+
 		}
 	sampled_cols++;
 	}
+
 }
